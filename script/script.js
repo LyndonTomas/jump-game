789ec85c6f97bad6d8b846f0d35fb435fff1b2d8
LyndonTomas <201801180@iacademy.edu.ph>
2024-01-06 15:06:24 +0800

    add: added game score and game logic adjustment

--- script/script.js
+++ script/script.js
@@ -1,58 +1,14 @@
-/*
-let startScore = 0;
-
-document.addEventListener('keydown', function(event){
-    if(event.code === 'Space'){
-        jump();
-        // myFunction();
-    }
-});
-
-// function myFunction(){
-//     console.log("Spacebar pressed!");
-// };
-
-let character = document.getElementById("character");
-let block = document.getElementById("block");
-
-function jump(){
-    character.classList.add("animate");
-    setTimeout(function(){
-        
-        if(character.classList != "animate"){
-            character.classList.add("animate");
-        }
-
-        character.classList.remove("animate");
-    }, 500);
-
-    startScore++;
-    console.log(startScore);
-    
-}
-
-let checkDead = setTimeout(function(){
-    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-
-    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-
-    if(blockLeft < 20 && blockLeft > 0 && characterTop >= 130 ){
-        block.style.animation = "none";
-        block.style.display = "none";
-        alert(`Game Over! Your score is ${startScore}`);
-    }
-}, 10);
-*/
-
 var character = document.getElementById("character");
 var block = document.getElementById("block");
-var counter=0;
+var counter = 0;
+let points = 0;
+var score = document.getElementById("score");
 
-document.addEventListener('keydown', function(event){
-    if(event.code === 'Space'){
-        jump();
-    }
-});
+// document.addEventListener('keydown', function(event){
+//     if(event.code === 'Space'){
+//         jump();
+//     }
+// });
 
 function jump(){
     if(character.classList == "animate"){return}
@@ -61,18 +17,22 @@ function jump(){
         character.classList.remove("animate");
     },300);
     counter++;
+    points++;
     console.log(counter);
+    score.innerHTML = points;
 }
 var checkDead = setInterval(function() {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if(blockLeft <= 20 && blockLeft > 0 && characterTop >=130){
+    if(blockLeft <= 20 && blockLeft > 0 && characterTop >= 130){
         block.style.animation = "none";
-        alert("Game Over. score: "+Math.floor(counter/100));
+        alert(`Game Over. Your Final Score: "+ Math.floor(counter/100)`);
+        points = 0;
         counter=0;
         block.style.animation = "block 1s infinite linear";
     }else{
         counter++;
-        document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
+        score.innerHTML = points;
+        // document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
     }
 }, 10);
